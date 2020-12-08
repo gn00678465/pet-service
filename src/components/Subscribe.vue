0523688cb5b4090cd7a7ffa8940718b382240550
MADAO <gn00678465@gmail.com>
2020-12-08 10:59:28 +0800

    訂閱區塊 初步完成 & 建立 form 相關 components

--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="bg-light py-9">
     <div class="container">
-      <h2 class="h2 mb-2 text-primary">有任何我們能幫助您的嗎？</h2>
-      <p class="text-lg mb-4 font-medium  text-primary">輸入電子信箱<br />將有專業人員在線幫您解決所有問題</p>
+      <h2 class="text-xl h2 mb-3 text-primary md:text-4xl md:mb-2">有任何我們能幫助您的嗎？</h2>
+      <p class="md:text-lg mb-4 font-medium  text-primary">輸入電子信箱<br />將有專業人員在線幫您解決所有問題</p>
       <div class="row">
-        <div class="col-12 md:col-6">
+        <div class="col-12 md:col-9 lg:col-6">
+          <div class="form-group">
+            <input type="text" class="form-control py-4" placeholder="example@gmail.com" />
+            <button class="btn btn-primary col-4">送出</button>
+          </div>
         </div>
       </div>
     </div>
