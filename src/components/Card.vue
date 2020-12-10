f908031ef1b382e968d608b0489884a2aa94cd87
MADAO <gn00678465@gmail.com>
2020-12-10 20:41:01 +0800

    service card 元件化

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <router-link to="/"
+    class="card shadow min-h-full transform-gpu hover:scale-105 transition-transform">
+    <div class="card__img__top h-32 bg-cover bg-center"
+      :style="{backgroundImage: `url(${imgUrl})`}"></div>
+    <div class="card__body bg-gray-100 pt-2 pb-5 px-4 text-primary">
+      <h5 class="mb-1.5">{{ name }}</h5>
+      <div class="mb-1.5">
+        <template v-for="num in rating">
+          <span class="material-icons text-base text-light mr-1" :key="num">star</span>
+        </template>
+      </div>
+      <p class="text-sm sm:mb-5">{{ service.join('、') }}</p>
+    </div>
+    <div class="card__footer">
+      <slot name="footer"></slot>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    // path: {
+    //   type: String,
+    //   required: true,
+    // },
+    name: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    service: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/homestay/Service.vue
+++ src/views/homestay/Service.vue
@@ -3,25 +3,21 @@
     <ul class="row -mb-3 sm:-mb-5 md:-mb-8">
       <li class="col-6 mb-3 sm:col-4 sm:mb-5 lg:col-3 md:mb-8"
         v-for="item of homestayListsArr" :key="item.name">
-        <a class="card shadow min-h-full transform-gpu hover:scale-105 transition-transform">
-          <div class="card__img__top h-32 bg-cover bg-center"
-            :style="{backgroundImage: `url(${item.image})`}"></div>
-          <div class="card__body bg-gray-100 pt-2 pb-5 px-4 text-primary">
-            <h5 class="mb-1.5">{{ item.name }}</h5>
-            <div class="mb-1.5">
-              <template v-for="num in item.rating">
-                <span class="material-icons text-base text-light mr-1" :key="num">star</span>
-              </template>
-            </div>
-            <p class="text-sm sm:mb-5">{{ item.service.join('、') }}</p>
-          </div>
-          <div class="card__footer">
-            <div class="btn-groups">
-              <button class="btn btn-outline-primary w-1/2 rounded-tl-none">聯繫</button>
-              <button class="btn btn-outline-primary w-1/2 text-orange-400">預約</button>
-            </div>
-          </div>
-        </a>
+        <card
+        :path="item.id"
+        :name="item.name"
+        :imgUrl="item.image"
+        :rating="item.rating"
+        :service="item.service"
+        >
+        <template #footer>
+          <div class="btn-groups">
+          <button class="btn w-1/2 rounded-tl-none text-primary
+          hover:bg-gray-200">聯繫</button>
+          <button class="btn w-1/2 text-orange-400 hover:bg-gray-200">預約</button>
+        </div>
+        </template>
+        </card>
       </li>
     </ul>
     <pagination :totalPage="3" :currentPage.sync="page"
@@ -32,71 +28,15 @@
 
 <script>
 import pagination from '@/components/Pagination.vue';
+import card from '@/components/Card.vue';
 import data from '@/assets/data.json';
 
 export default {
   components: {
     pagination,
+    card,
   },
   data: () => ({
-    serviceLists: [
-      {
-        imgUrl: require('../../assets/images/homestay-2.jpeg'),
-        name: '寵屋屋',
-        rating: 5,
-        content: ['寄宿', '美容洗澡', '到府保姆'],
-        path: '',
-      },
-      {
-        imgUrl: require('../../assets/images/homestay-3.jpeg'),
-        name: '毛孩好家',
-        rating: 5,
-        content: ['寄宿', '美容洗澡', '到府保姆'],
-        path: '',
-      },
-      {
-        imgUrl: require('../../assets/images/homestay-4.jpeg'),
-        name: '妖貓X僕SS',
-        rating: 5,
-        content: ['寄宿', '美容洗澡'],
-        path: '',
-      },
-      {
-        imgUrl: require('../../assets/images/homestay-5.jpeg'),
-        name: '貓是主我是僕',
-        rating: 5,
-        content: ['寄宿', '美容洗澡', '到府保姆'],
-        path: '',
-      },
-      {
-        imgUrl: require('../../assets/images/homestay-6.jpeg'),
-        name: '汪汪隊',
-        rating: 5,
-        content: ['寄宿', '到府保姆', '寵物行為訓練'],
-        path: '',
-      },
-      {
-        imgUrl: require('../../assets/images/homestay-7.jpeg'),
-        name: '卯咪',
-        rating: 5,
-        content: ['寄宿', '寵物行為訓練'],
-        path: '',
-      },
-      {
-        imgUrl: require('../../assets/images/index-2.jpeg'),
-        name: '貓狗大戰',
-        rating: 5,
-        content: ['寄宿', '美容洗澡', '到府保姆'],
-        path: '',
-      },
-      {
-        imgUrl: require('../../assets/images/homestay-8.jpeg'),
-        name: '萌寵底家',
-        rating: 5,
-        content: ['寄宿', '到府保姆'],
-        path: '',
-      },
-    ],
     homestayLists: [],
     page: 1,
   }),
