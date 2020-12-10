070889fb73f2e0d9fdb5ae8d0673588b3b0bf250
MADAO <gn00678465@gmail.com>
2020-12-10 21:02:58 +0800

    修正 footer 連結

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,10 +3,20 @@
     <h2 class="logo md:hidden">PETPET</h2>
     <div class="hidden md:grid grid-cols-4 gap-7 items-baseline mb-24">
       <ul v-for="items of footerLinks" :key="items.title">
-        <li><h4 :class="items.titleClass">{{ items.title }}</h4></li>
+        <li>
+          <h4 :class="items.titleClass">{{ items.title }}</h4>
+        </li>
         <template v-for="link of items.links" >
-          <li :key="link"><a :class="items.linkClass" href="#">{{ link }}</a></li>
+          <li :key="link">
+            <router-link class="inline-block py-3" :to="link.path">{{ link.link }}</router-link>
+          </li>
         </template>
+      </ul>
+      <ul>
+        <li>
+          <h4 class="h6 mb-9">聯繫我們</h4>
+          <a href="mailto:petpet@petmail.com" class="inline-block py-3">petpet@petmail.com</a>
+        </li>
       </ul>
     </div>
     <div class="flex justify-end md:justify-between items-center">
@@ -27,26 +37,68 @@ export default {
         {
           title: 'PETPET',
           titleClass: 'logo mb-6',
-          links: ['關於我們', '幫助中心', '使用條款', '隱私政策'],
-          linkClass: 'inline-block py-3',
+          links: [
+            {
+              link: '關於我們',
+              path: '/',
+            },
+            {
+              link: '幫助中心',
+              path: '/',
+            },
+            {
+              link: '使用條款',
+              path: '/',
+            },
+            {
+              link: '隱私政策',
+              path: '/',
+            },
+          ],
         },
         {
           title: '寵物服務',
           titleClass: 'h6 mb-9',
-          links: ['寵物寄宿', '寵物到府保姆', '寵物美容洗澡', '寵物行為訓練'],
-          linkClass: 'inline-block py-3',
+          links: [
+            {
+              link: '寵物寄宿',
+              path: 'homestay',
+            },
+            {
+              link: '寵物到府保姆',
+              path: '/',
+            },
+            {
+              link: '寵物美容洗澡',
+              path: '/',
+            },
+            {
+              link: '寵物行為訓練',
+              path: '/',
+            },
+          ],
         },
         {
           title: '加入我們',
           titleClass: 'h6 mb-9',
-          links: ['成為寵物保姆', '加入寵物計程車', '專屬 APP 下載', '相關合約條款'],
-          linkClass: 'inline-block py-3',
-        },
-        {
-          title: '聯繫我們',
-          titleClass: 'h6 mb-9',
-          links: ['petpet@petmail.com'],
-          linkClass: 'inline-block py-3',
+          links: [
+            {
+              link: '成為寵物保姆',
+              path: '/',
+            },
+            {
+              link: '加入寵物計程車',
+              path: '/',
+            },
+            {
+              link: '專屬 APP 下載',
+              path: '/',
+            },
+            {
+              link: '相關合約條款',
+              path: '/',
+            },
+          ],
         },
       ],
     };
